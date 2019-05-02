02727bb17d33a84a3d68d8707ca3756ab511fa6f
Michael Marvive <marvive42@gmail.com>
2019-05-01 17:55:48 -0700

    Updates to model and view

--- src/scheduler/view/ReportsController.java
+++ src/scheduler/view/ReportsController.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import scheduler.model.Appointment;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class ReportsController {
 
@@ -11,22 +15,22 @@ public class ReportsController {
     private Tab appointmentTab;
 
     @FXML
-    private TableView<?> appointmentTableView;
+    private TableView<Appointment> appointmentTableView;
 
     @FXML
-    private TableColumn<?, ?> appointmentTitleColumn;
+    private TableColumn<Appointment, String> appointmentTitleColumn;
 
     @FXML
-    private TableColumn<?, ?> appointmentTypeColumn;
+    private TableColumn<Appointment, String> appointmentTypeColumn;
 
     @FXML
-    private TableColumn<?, ?> appointmentStartColumn;
+    private TableColumn<Appointment, ?> appointmentStartColumn;
 
     @FXML
-    private TableColumn<?, ?> appointmentEndColumn;
+    private TableColumn<Appointment, ?> appointmentEndColumn;
 
     @FXML
-    private TableColumn<?, ?> appointmentCustomerColumn;
+    private TableColumn<Appointment, String> appointmentCustomerColumn;
 
     @FXML
     private Tab consultantTab;
@@ -35,17 +39,58 @@ public class ReportsController {
     private TableView<?> scheduleTableView;
 
     @FXML
-    private TableColumn<?, ?> scheduleMonthColumn;
+    private TableColumn<Appointment, ?> scheduleMonthColumn;
 
     @FXML
-    private TableColumn<?, ?> scheduleTypeColumn;
+    private TableColumn<Appointment, ?> scheduleTypeColumn;
 
     @FXML
-    private TableColumn<?, ?> scheduleAmountColumn;
+    private TableColumn<Appointment, ?> scheduleAmountColumn;
 
     @FXML
     private Tab reportTab;
 
+    private void setLanguage() {
+        ResourceBundle rb = ResourceBundle.getBundle("reports", Locale.getDefault());
+        appointmentTitleColumn.setText(rb.getString("appointmentTitleColumn"));
+        appointmentTypeColumn.setText(rb.getString("appointmentTypeColumn"));
+        appointmentStartColumn.setText(rb.getString("appointmentStartColumn"));
+        appointmentEndColumn.setText(rb.getString("appointmentEndColumn"));
+        appointmentCustomerColumn.setText(rb.getString("appointmentCustomerColumn"));
+        scheduleMonthColumn.setText(rb.getString("scheduleMonthColumn"));
+        scheduleTypeColumn.setText(rb.getString("scheduleTypeColumn"));
+        scheduleAmountColumn.setText(rb.getString("scheduleAmountColumn"));
+        appointmentTab.setText(rb.getString("appointmentTab"));
+        consultantTab.setText(rb.getString("consultantTab"));
+        reportTab.setText(rb.getString("reportTab"));
+    }
+
+    @FXML
+    public void initialize() {
+//        Sets the local Language
+        setLanguage();
+        // Assign actions to buttons
+        btnModifyCustomerSave.setOnAction(event -> saveModifyCustomer(event));
+        btnModifyCustomerCancel.setOnAction(event -> cancelModifyCustomer(event));
+        // Get customer to be modified via index
+        customer = getCustomerRoster().get(customerIndexToModify);
+        // Get customer information
+        String customerName = customer.getCustomerName();
+        String address = customer.getAddress();
+        String address2 = customer.getAddress2();
+        String city = customer.getCity();
+        String country = customer.getCountry();
+        String postalCode = customer.getPostalCode();
+        String phone = customer.getPhone();
+        // Populate information fields with current customer information
+        txtModifyCustomerName.setText(customerName);
+        txtModifyCustomerAddress.setText(address);
+        txtModifyCustomerAddress2.setText(address2);
+        txtModifyCustomerCity.setText(city);
+        txtModifyCustomerCountry.setText(country);
+        txtModifyCustomerPostalCode.setText(postalCode);
+        txtModifyCustomerPhone.setText(phone);
+    }
 
 }
 
