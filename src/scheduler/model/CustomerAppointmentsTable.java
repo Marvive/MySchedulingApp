c762e95b4e0a7b7c57b7d81bfed388c2bb590a68
Michael Marvive <marvive42@gmail.com>
2019-05-26 10:03:12 -0700

    Version 0.8.6
    -Created final report
    
    Known Bugs:
    
    Next Steps:
    -Delete everything from Barchart
    -create resourcebundle info for the new table tab and Columns

---
+++ src/scheduler/model/CustomerAppointmentsTable.java
@@ -0,0 +1,71 @@
+package scheduler.model;
+
+import javafx.beans.property.*;
+
+public class CustomerAppointmentsTable {
+
+    private StringProperty customer;
+    private StringProperty consultation;
+    private StringProperty followUp;
+    private StringProperty newAccount;
+    private StringProperty closeAccount;
+    private StringProperty total;
+
+    public CustomerAppointmentsTable(String customer, String counter1, String counter2, String counter3, String counter4, String counterTotal) {
+        this.customer = new SimpleStringProperty(customer);
+        this.consultation = new SimpleStringProperty(counter1);
+        this.followUp = new SimpleStringProperty(counter2);
+        this.newAccount = new SimpleStringProperty(counter3);
+        this.closeAccount = new SimpleStringProperty(counter4);
+        this.total = new SimpleStringProperty(counterTotal);
+    }
+
+
+    public String getCustomer() {
+        return customer.get();
+    }
+
+    public StringProperty customerProperty() {
+        return customer;
+    }
+
+    public String getConsultation() {
+        return consultation.get();
+    }
+
+    public StringProperty consultationProperty() {
+        return consultation;
+    }
+
+    public String getFollowUp() {
+        return followUp.get();
+    }
+
+    public StringProperty followUpProperty() {
+        return followUp;
+    }
+
+    public String getNewAccount() {
+        return newAccount.get();
+    }
+
+    public StringProperty newAccountProperty() {
+        return newAccount;
+    }
+
+    public String getCloseAccount() {
+        return closeAccount.get();
+    }
+
+    public StringProperty closeAccountProperty() {
+        return closeAccount;
+    }
+
+    public String getTotal() {
+        return total.get();
+    }
+
+    public StringProperty totalProperty() {
+        return total;
+    }
+}
--- src/scheduler/view/ReportsController.java
+++ src/scheduler/view/ReportsController.java
@@ -7,10 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -18,6 +14,7 @@ import javafx.stage.Stage;
 import scheduler.model.Appointment;
 import scheduler.model.AppointmentList;
 import scheduler.model.AppointmentTypesByMonthTable;
+import scheduler.model.CustomerAppointmentsTable;
 import scheduler.util.DatabaseConnection;
 
 import java.io.IOException;
@@ -64,6 +61,32 @@ public class ReportsController {
     @FXML
     private TableColumn<AppointmentTypesByMonthTable, String> scheduleAmountColumn;
 
+
+    @FXML
+    private Tab customerAppointmentTab;
+
+    @FXML
+    private TableView<CustomerAppointmentsTable> customerAppointmentsTableView;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> cACustomerColumn;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> cAConsultationColumn;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> cAFollowUpColumn;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> cANewAccountColumn;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> cACloseAccountColumn;
+
+    @FXML
+    private TableColumn<CustomerAppointmentsTable, String> caTotalColumn;
+
+
     @FXML
     private Tab reportTab;
 
@@ -107,24 +130,26 @@ public class ReportsController {
     @FXML
     private Menu menuBarFile;
 
-//    private final CategoryAxis xAxis = new CategoryAxis();
-//    private final NumberAxis yAxis = new NumberAxis();
 
-    @FXML
-    private BarChart<String, Number> barChart;
-    @FXML
-    private CategoryAxis xAxis;
-    @FXML
-    private NumberAxis yAxis;
-
-
-//    CategoryAxis xAxis    = new CategoryAxis();
-//xAxis.setLabel("Devices");
+////    private final CategoryAxis xAxis = new CategoryAxis();
+////    private final NumberAxis yAxis = new NumberAxis();
 //
-//    NumberAxis yAxis = new NumberAxis();
-//yAxis.setLabel("Visits");
+//    @FXML
+//    private BarChart<String, Integer> barChart;
+//    @FXML
+//    private CategoryAxis xAxis;
+//    @FXML
+//    private NumberAxis yAxis;
 //
-//    BarChart     barChart = new BarChart(xAxis, yAxis);
+//
+////    CategoryAxis xAxis    = new CategoryAxis();
+////xAxis.setLabel("Devices");
+////
+////    NumberAxis yAxis = new NumberAxis();
+////yAxis.setLabel("Visits");
+////
+////    BarChart     barChart = new BarChart(xAxis, yAxis);
+//    private HashMap<String, Integer> hashmap;
 
     /**
      * Menu Handlers
@@ -416,133 +441,26 @@ public class ReportsController {
     /**
      * Sets a bar chart of how many appointments each customer has
      */
-    @FXML
-    private void setBarChart() {
-        DatabaseConnection.updateAppointmentList();
-        ObservableList<XYChart.Data<String, Integer>> data1 = FXCollections.observableArrayList();
-        ObservableList<XYChart.Data<String, Integer>> data2 = FXCollections.observableArrayList();
-        ObservableList<XYChart.Data<String, Integer>> data3 = FXCollections.observableArrayList();
-        ObservableList<XYChart.Data<String, Integer>> data4 = FXCollections.observableArrayList();
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
-
-        ArrayList<String> customerNamesWithAppointments = new ArrayList<>();
-//        Setting the Categories
-        for (Appointment appointment : AppointmentList.getAppointmentList()) {
-            String name = appointment.getContact();
-            if (!(customerNamesWithAppointments.contains(name))) {
-                customerNamesWithAppointments.add(name);
-            }
-        }
-
-//        for (Appointment appointment: getAppointmentList()) {
-//            String type = appointment.getType();
-//            int counter = 0;
-//            for (String customer : customerNamesWithAppointments) {
-//                if (appointment.getContact().equals(customer)) {
-//                    counter++;
-//                }
-//            }
-//
-//            data.add(new XYChart.Data<>(type, counter));
-//
-//        }
-//
-//        System.out.println(data);
-
-        for (String customer : customerNamesWithAppointments) {
-            int counter1 = 0;
-            int counter2 = 0;
-            int counter3 = 0;
-            int counter4 = 0;
-            String type1 = "";
-            String type2 = "";
-            String type3 = "";
-            String type4 = "";
-            for (Appointment appointment : AppointmentList.getAppointmentList()) {
-                if (customer.equals(appointment.getContact())) {
-                    if (appointment.getType().equals(consultationString)) {
-                        type1 = appointment.getType();
-                        counter1++;
-
-                    } else if (appointment.getType().equals(followUpString)) {
-                        type2 = appointment.getType();
-                        counter2++;
-
-                    } else if (appointment.getType().equals(newAccountString)) {
-                        type3 = appointment.getType();
-                        counter3++;
-
-                    } else if (appointment.getType().equals(closeAccountString)) {
-                        type4 = appointment.getType();
-                        counter4++;
-                    }
-                }
-            }
-            if (!type1.equals("")) {
-                data1.add(new XYChart.Data<>(type1, counter1, customer));
-            }
-            if (!type2.equals("")) {
-                data2.add(new XYChart.Data<>(type2, counter2, customer));
-            }
-            if (!type3.equals("")) {
-                data3.add(new XYChart.Data<>(type3, counter3, customer));
-            }
-            if (!type4.equals("")) {
-                data4.add(new XYChart.Data<>(type4, counter4, customer));
-            }
-        }
-        System.out.println(data1);
-        System.out.println(data2);
-        System.out.println(data3);
-        System.out.println(data4);
-    }
-
-
-
-
-
-//        for (Appointment appointment: AppointmentList.getAppointmentList()) {
-//            int counter1 = 0;
-//            int counter2 = 0;
-//            int counter3 = 0;
-//            int counter4 = 0;
-//            String type1 ="";
-//            String type2 ="";
-//            String type3 ="";
-//            String type4 ="";
-//            for (String customer : customerNamesWithAppointments) {
-//                if (appointment.getContact().equals(customer)) {
-//                    System.out.print(customer + " ");
-//                    if (appointment.getType().equals(consultationString)) {
-//                        type1 = appointment.getType();
-//                        counter1++;
-//
-//                    } else if (appointment.getType().equals(followUpString)) {
-//                        counter2++;
-//
-//                    } else if (appointment.getType().equals(newAccountString)) {
-//                        counter3++;
-//
-//                    } else if (appointment.getType().equals(closeAccountString)) {
-//                        counter4++;
-//                    }
-//                }
-//                if (!type1.equals("")) {
-//                    data.add(new XYChart.Data<>(type1, counter1));
-//                }
-//            }
-//
-//
-//        }
-//        System.out.println();
-
-
-//        data.addAll(series);
-//        barChart.getData().add(series);
-
-
-////        ResourceBundle rb = ResourceBundle.getBundle("resources/reports", Locale.getDefault());
+//    @FXML
+//    private void setBarChart() {
 //        DatabaseConnection.updateAppointmentList();
+//        ObservableList<XYChart.Data<String, Integer>> data1 = FXCollections.observableArrayList();
+//        ObservableList<XYChart.Data<String, Integer>> data2 = FXCollections.observableArrayList();
+//        ObservableList<XYChart.Data<String, Integer>> data3 = FXCollections.observableArrayList();
+//        ObservableList<XYChart.Data<String, Integer>> data4 = FXCollections.observableArrayList();
+//
+//
+////        private HashMap<String, Integer> hashmap;
+//
+//        XYChart.Series series1 = new XYChart.Series();
+//        XYChart.Series<String, Integer> series2 = new XYChart.Series();
+//        XYChart.Series<String, Integer> series3 = new XYChart.Series();
+//        XYChart.Series<String, Integer> series4 = new XYChart.Series();
+//
+////        XYChart.Series series1 = new XYChart.Series();
+////        XYChart.Series series2 = new XYChart.Series();
+////        XYChart.Series series3 = new XYChart.Series();
+////        XYChart.Series series4 = new XYChart.Series();
 //
 ////        Axis Labels
 //        xAxis.setLabel(rb1.getString("customers"));
@@ -551,17 +469,110 @@ public class ReportsController {
 //
 //        ArrayList<String> customerNamesWithAppointments = new ArrayList<>();
 ////        Setting the Categories
-//        for (Appointment appointment: AppointmentList.getAppointmentList()) {
+//        for (Appointment appointment : AppointmentList.getAppointmentList()) {
 //            String name = appointment.getContact();
-//            if (!(customerNamesWithAppointments.contains(name))){
+//            if (!(customerNamesWithAppointments.contains(name))) {
 //                customerNamesWithAppointments.add(name);
 //            }
 //        }
 //
 //        xAxis.setCategories(FXCollections.observableArrayList(customerNamesWithAppointments));
+//        xAxis.invalidateRange(customerNamesWithAppointments);
+//        series1.setName("Current Test");
 //
 //
+////        barChart.getData().add(series1);
 //
+//
+//        for (String customer : customerNamesWithAppointments) {
+//            int counter1 = 0;
+//            int counter2 = 0;
+//            int counter3 = 0;
+//            int counter4 = 0;
+//            String type1 = "";
+//            String type2 = "";
+//            String type3 = "";
+//            String type4 = "";
+//            for (Appointment appointment : AppointmentList.getAppointmentList()) {
+//                if (customer.equals(appointment.getContact())) {
+//                    if (appointment.getType().equals(consultationString)) {
+//                        type1 = appointment.getType();
+//                        counter1++;
+//
+//                    } else if (appointment.getType().equals(followUpString)) {
+//                        type2 = appointment.getType();
+//                        counter2++;
+//
+//                    } else if (appointment.getType().equals(newAccountString)) {
+//                        type3 = appointment.getType();
+//                        counter3++;
+//
+//                    } else if (appointment.getType().equals(closeAccountString)) {
+//                        type4 = appointment.getType();
+//                        counter4++;
+//                    }
+//                }
+//            }
+//            if (!type1.equals("")) {
+////                data1.add(new XYChart.Data<>(type1, counter1, customer));
+//                series1.getData().add(new XYChart.Data<>(type1, counter1));
+//                hashmap.put(type1, counter1);
+//            }
+//            if (!type2.equals("")) {
+////                data2.add(new XYChart.Data<>(type2, counter2, customer));
+//                series2.getData().add(new XYChart.Data<>(type2, counter2));
+//            }
+//            if (!type3.equals("")) {
+////                data3.add(new XYChart.Data<>(type3, counter3, customer));
+//                series3.getData().add(new XYChart.Data<>(type3, counter3));
+//            }
+//            if (!type4.equals("")) {
+////                data4.add(new XYChart.Data<>(type4, counter4, customer));
+//                series4.getData().add(new XYChart.Data<>(type4, counter4));
+//            }
+//        }
+//
+//
+//        barChart.getData().add(series1);
+//
+//
+//        System.out.println(hashmap);
+////        System.out.println(data1);
+////        System.out.println(data2);
+////        System.out.println(data3);
+////        System.out.println(data4);
+////        System.out.println(series1);
+////        System.out.println(series2);
+////        System.out.println(series3);
+////        System.out.println(series4);
+//
+////        barChart.getData().addAll(data1);
+////        barChart.getData().addAll(series1, series2, series3, series4);
+////
+////        barChart.getData().add(new XYChart.Series<>(consultationString, data1));
+////        barChart.getData().add(new XYChart.Series<>(consultationString, data1));
+////        barChart.getData().add(new XYChart.Series<>(followUpString, data2));
+////        barChart.getData().add(new XYChart.Series<>(newAccountString, data3));
+////        barChart.getData().add(new XYChart.Series<>(closeAccountString, data4));
+////
+////
+////        while (true) {
+////            String title = appointmentgetTitle();
+////            int counter = 0;
+////            for (:
+////                 ) {
+////
+////            }
+////
+////            data.add(new XYChart.Data<>(title, counter));
+////            counter++;
+//    }
+
+
+//        data.addAll(series);
+//        barChart.getData().add(series);
+
+
 //
 ////        new XYChart.Data<>(item.getName(), item.getValue1()))
 //        ObservableList<XYChart.Data<String, Number>> data = FXCollections.observableArrayList();
@@ -606,7 +617,86 @@ public class ReportsController {
 //}
 
 
+    /**
+     * Each row will be a customer showing types of appointments and the total
+     */
     @FXML
+    private void setCustomerAppointmentsTableView() {
+        DatabaseConnection.updateAppointmentList();
+
+
+        ArrayList<String> customerNamesWithAppointments = new ArrayList<>();
+//        Setting the Categories
+        for (Appointment appointment : AppointmentList.getAppointmentList()) {
+            String name = appointment.getContact();
+            if (!(customerNamesWithAppointments.contains(name))) {
+                customerNamesWithAppointments.add(name);
+            }
+        }
+
+        final ObservableList<CustomerAppointmentsTable> caData = FXCollections.observableArrayList();
+        for (String customer : customerNamesWithAppointments) {
+            int counter1 = 0;
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            int counterTotal = 0;
+//            String type1 = "";
+//            String type2 = "";
+//            String type3 = "";
+//            String type4 = "";
+            for (Appointment appointment : AppointmentList.getAppointmentList()) {
+                if (customer.equals(appointment.getContact())) {
+                    if (appointment.getType().equals(consultationString)) {
+//                        type1 = appointment.getType();
+                        counter1++;
+
+                    } else if (appointment.getType().equals(followUpString)) {
+//                        type2 = appointment.getType();
+                        counter2++;
+
+                    } else if (appointment.getType().equals(newAccountString)) {
+//                        type3 = appointment.getType();
+                        counter3++;
+
+                    } else if (appointment.getType().equals(closeAccountString)) {
+//                        type4 = appointment.getType();
+                        counter4++;
+                    }
+                    counterTotal++;
+                }
+
+            }
+//            if (!type1.equals("")) {
+//
+//            }
+//            if (!type2.equals("")) {
+//
+//            }
+//            if (!type3.equals("")) {
+//
+//            }
+//            if (!type4.equals("")) {
+//
+//            }
+            String consultationCounter = String.valueOf(counter1);
+            String followUpCounter = String.valueOf(counter2);
+            String newAccountCounter = String.valueOf(counter3);
+            String closeAccountCounter = String.valueOf(counter4);
+            String totalCounter = String.valueOf(counterTotal);
+            caData.add(new CustomerAppointmentsTable(customer, consultationCounter, followUpCounter, newAccountCounter, closeAccountCounter, totalCounter));
+        }
+
+
+        customerAppointmentsTableView.setItems(caData);
+        cACustomerColumn.setCellValueFactory(cellData -> cellData.getValue().customerProperty());
+        cAConsultationColumn.setCellValueFactory(cellData -> cellData.getValue().consultationProperty());
+        cAFollowUpColumn.setCellValueFactory(cellData -> cellData.getValue().followUpProperty());
+        cANewAccountColumn.setCellValueFactory(cellData -> cellData.getValue().newAccountProperty());
+        cACloseAccountColumn.setCellValueFactory(cellData -> cellData.getValue().closeAccountProperty());
+        caTotalColumn.setCellValueFactory(cellData -> cellData.getValue().totalProperty());
+    }
+
     public void initialize() {
 //        Sets the local Language
         setLanguage();
@@ -616,7 +706,9 @@ public class ReportsController {
         setConsultantScheduleTableView();
 
 //        Sets the Bar Chart Data
-        setBarChart();
+//        setBarChart();
+
+        setCustomerAppointmentsTableView();
     }
 
 }
